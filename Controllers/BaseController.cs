73121c2ed03cc3c1c1ae3015e49e57dd9ccb6caf
icibi <icibi@my.centennialcollege.ca>
2024-11-07 16:34:42 -0500

    Created a login session for users and temporary sign out button.

---
+++ Controllers/BaseController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace lab3app.Controllers
+{
+    public class BaseController : Controller
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var userId = HttpContext.Session.GetInt32("UserId");
+            if (userId == null)
+            {
+                //Redirect to the login page if the user is not logged in
+                context.Result = RedirectToAction("Login", "Account");
+            }
+            base.OnActionExecuting(context);
+        }
+    }
+}
